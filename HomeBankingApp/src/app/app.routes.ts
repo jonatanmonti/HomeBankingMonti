f883047855666fb5b489da05db588308686bbf37
jonatan monti <51387626+jonatanmonti@users.noreply.github.com>
2025-07-15 14:18:00 -0300

    Login front
    Se crea la parte visual del login

--- HomeBankingApp/src/app/app.routes.ts
+++ HomeBankingApp/src/app/app.routes.ts
@@ -2,12 +2,12 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+    path: 'login',
+    loadComponent: () => import('./login/login.page').then((m) => m.LoginPage),
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'login',
     pathMatch: 'full',
   },
 ];
